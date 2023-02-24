dd31c28eabf2ce680514571093b6fafae2eacf5f
Yoda <yoda@yodacode.xyz>
2023-02-24 08:20:21 -0800

    fix registration

--- templates/register.js
+++ templates/register.js
@@ -1,6 +1,6 @@
 export const required = ["firstName"];
 
-export const template = ({ firstName }) => `Thanks for registering, ${firstName}! We'll reach out to you soon to sign the event liability waiver. In the meantime, join our <a href="https://hackoc.org/dis
+export const template = ({ firstName }) => `Thanks for registering, ${firstName}! We'll reach out to you soon to sign the event liability waiver. In the meantime, join our <a href="https://hackoc.org/discord">Discord server</a>
 <br/>
 <br/>
 Cheers,
@@ -14,7 +14,7 @@ Ian & the Hack OC team
 <br/>
 <small>We value your privacy and we'll never track whether you read or open our emails.</small>`;
 
-export const text = ({ firstName }) => `Thanks for registering, ${firstName}! We'll reach out to you soon to sign the event liability waiver. In the meantime, join our Discord server (https://hackoc.org/
+export const text = ({ firstName }) => `Thanks for registering, ${firstName}! We'll reach out to you soon to sign the event liability waiver. In the meantime, join our Discord server (https://hackoc.org/discord) to chat with other hackers.
 Cheers,
 Ian & the Hack OC team
 -----
